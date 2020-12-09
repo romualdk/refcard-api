6ff767de677dc3bf943a530a06ea15fbd794575f
romualdk <romuald.kowalczyk@gmail.com>
2020-12-09 22:41:37 +0100

    comments

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+
+:: Prepare
 if not exist _release mkdir _release
 cd _release
 
@@ -5,22 +7,31 @@ if exist current rmdir current /s /q
 mkdir current
 cd current
 
+:: Clone
 git clone https://github.com/romualdk/refcard-backend.git .
 
+:: Get version
 git rev-list --count HEAD > version.txt
 set /P version=<version.txt
 
 set prefix=refcard-backend-v
 set tag=%prefix%%version%
 
+:: Rename to version
 cd ..
 if exist %tag% rmdir %tag% /s /q
 rename current %tag%
 cd %tag%
 
+:: Install
 call composer install --no-dev --prefer-dist --optimize-autoloader
 
+:: Zip
 cd ..
 tar -a -c -f %tag%.zip %tag%
 
+:: Clean
+rmdir %tag% /s /q
+
+:: Done
 echo %tag%
